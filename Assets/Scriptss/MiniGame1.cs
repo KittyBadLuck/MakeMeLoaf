e5c1e8231446aaef468f766521cc57d26de304af
Geoff <g.carrot@rubika-edu.ca>
2024-01-28 00:21:27 -0500

    Update du mini jeu

--- Assets/Scriptss/MiniGame1.cs
+++ Assets/Scriptss/MiniGame1.cs
@@ -6,8 +6,7 @@ using UnityEngine.UI;
 
 public class MiniGame1 : MonoBehaviour
 {
-    private float leftIntensity;
-    public Slider paw;
+    public Transform doughShape
 
     // Start is called before the first frame update
     void Start()
@@ -18,22 +17,7 @@ public class MiniGame1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       // Debug.Log("the left trigger is at "+leftIntensity);
-       paw.value = leftIntensity;
-       Debug.Log("paw is at "+paw.value);
-       //Debug.Log(leftIntensity);  
-       if(paw.value.GetType() == typeof(float))
-       {
-           Debug.Log("ta mere");
-       }
+         
     }
-
-    public void LeftHand(InputAction.CallbackContext context)
-    {
-        leftIntensity = context.ReadValue<float>();
-          
-        // paw.value = leftIntensity;
-    }
-
 
 }
